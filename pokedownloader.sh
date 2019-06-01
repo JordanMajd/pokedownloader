da829dcbe6842c59315f9ae80926f6a84b0c9c0c
Jordan Majd <jordan@madtitanstudios.com>
2019-06-01 11:59:01 -0600

    Improvements: - Use static asset URL. - Respect rate limiting. - Name images after pokemon. - Save to images directory. - Supress CURL output.

--- pokedownloader.sh
+++ pokedownloader.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# rate limit is 100 per minute
+RATE_LIMIT=$(bc <<< "scale=3; 60 / 100")
+echo $RATE_LIMIT
+
 echo "Fetching number of pokemon..."
-CT=$(curl https://pokeapi.co/api/v2/pokemon/ | cut -d ":" -f2 | cut -d "," -f1)
-echo "$CT pokemon found, downloading..."
+CT=$(curl -s https://pokeapi.co/api/v2/pokemon/ | cut -d ":" -f2 | cut -d "," -f1)
+echo "${CT} pokemon found, downloading..."
 for i in `seq 1 $CT`; do
+	NAME=$(curl -s https://pokeapi.co/api/v2/pokemon/${i}/ | grep -Po ',"name":.*?[^\\]"' | cut -d'"' -f 4)
+	echo "Downloading ${NAME}..."
 	#DO YOU EVEN LIFT?
-	curl https://www.pokeapi.co/media/img/$i.png > $i.png
+	curl -s https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png > images/${NAME}.png
+	sleep $RATE_LIMIT
 done
